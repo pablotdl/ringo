48b00df3b1ade2a49753179ec7bbbcb9cfaf4d69
Pablo Saavedra <pablo.a.saavedra@gmail.com>
2013-06-11 19:17:56 -0300

    Improvements to the agent api: - Added fields to the call info object - Added javadoc in the call info and the agent service - Improved status method to show 503 when the persistence service is not available

--- agent/src/main/java/ar/edu/unicen/ringo/agent/api/AgentService.java
+++ agent/src/main/java/ar/edu/unicen/ringo/agent/api/AgentService.java
@@ -12,36 +12,60 @@ import javax.ws.rs.core.Response.Status;
 import ar.edu.unicen.ringo.persistence.InvocationData;
 import ar.edu.unicen.ringo.persistence.PersistenceService;
 
+/**
+ * Service API exposed by the agent that accepts logging calls from different
+ * services.
+ *
+ * @author psaavedra
+ */
 @Path("/agent")
 public class AgentService {
 
-	private PersistenceService persistenceService;
+    /**
+     * The persistence service.
+     */
+    private volatile PersistenceService persistenceService;
 
-	@POST
-	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-	public Response monitor(CallInfo info) {
-		if (persistenceService == null) {
-			return Response.status(Status.SERVICE_UNAVAILABLE)
-					.entity("Service unavailable").build();
-		}
-		try {
-			InvocationData data = null;
-			this.persistenceService.persist(data);
-			return Response.ok().build();
-		} catch (RuntimeException e) {
-			return Response.status(Status.SERVICE_UNAVAILABLE)
-					.entity("Service unavailable").build();
-		}
- 	}
+    /**
+     * Logs a call from a service for later reporting.
+     * 
+     * @param info
+     *            The info about the call.
+     * @return A JAX-RS response object representing the result.
+     */
+    @POST
+    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Response monitor(CallInfo info) {
+        if (persistenceService == null) {
+            return Response.status(Status.SERVICE_UNAVAILABLE)
+                    .entity("Service unavailable").build();
+        }
+        try {
+            InvocationData data = null;
+            this.persistenceService.persist(data);
+            return Response.ok().build();
+        } catch (RuntimeException e) {
+            return Response.status(Status.SERVICE_UNAVAILABLE)
+                    .entity("Service unavailable").build();
+        }
+    }
 
-	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public Response status() {
-		return Response.ok("It's working").build();
-	}
+    /**
+     * Reports the status of the agent API.
+     * @return A JAX-RS response describing the status.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response status() {
+        if (this.persistenceService == null) {
+            return Response.status(Status.SERVICE_UNAVAILABLE)
+                    .entity("Persistence service not available").build();
+        }
+        return Response.ok("It's working. Persistence service is: " + this.persistenceService).build();
+    }
 
-	public void setPersistenceService(PersistenceService persistenceService) {
-		this.persistenceService = persistenceService;
-	}
+    public void setPersistenceService(PersistenceService persistenceService) {
+        this.persistenceService = persistenceService;
+    }
 
 }
--- agent/src/main/java/ar/edu/unicen/ringo/agent/api/CallInfo.java
+++ agent/src/main/java/ar/edu/unicen/ringo/agent/api/CallInfo.java
@@ -1,5 +1,62 @@
 package ar.edu.unicen.ringo.agent.api;
 
-public class CallInfo {
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Represents the information logged into the agent about a service call.
+ *
+ * @author psaavedra
+ */
+@XmlRootElement
+public class CallInfo implements Serializable {
+
+    /**
+     * Serial version.
+     */
+    private static final long serialVersionUID = -8547778257252654854L;
+
+    @XmlElement
+    private String sla;
+    @XmlElement
+    private String node;
+    @XmlElement
+    private String method;
+    @XmlElement
+    private int executionTime;
+    @XmlElement
+    private long timestamp;
+    public String getSla() {
+        return sla;
+    }
+    public void setSla(String sla) {
+        this.sla = sla;
+    }
+    public String getNode() {
+        return node;
+    }
+    public void setNode(String node) {
+        this.node = node;
+    }
+    public String getMethod() {
+        return method;
+    }
+    public void setMethod(String method) {
+        this.method = method;
+    }
+    public int getExecutionTime() {
+        return executionTime;
+    }
+    public void setExecutionTime(int executionTime) {
+        this.executionTime = executionTime;
+    }
+    public long getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
 
 }
