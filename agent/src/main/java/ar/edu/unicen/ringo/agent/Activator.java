19e26e277107e91369f6cc1c16e0ae27a3f16b32
Pablo Saavedra <pablo.a.saavedra@gmail.com>
2013-06-11 12:51:57 -0300

    Completed bundle activator implementation

--- agent/src/main/java/ar/edu/unicen/ringo/agent/Activator.java
+++ agent/src/main/java/ar/edu/unicen/ringo/agent/Activator.java
@@ -1,50 +1,39 @@
 package ar.edu.unicen.ringo.agent;
 
-import java.util.HashMap;
-
-import org.glassfish.jersey.servlet.ServletContainer;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
 import org.osgi.service.http.HttpService;
 import org.osgi.util.tracker.ServiceTracker;
 
 import ar.edu.unicen.ringo.agent.api.AgentService;
+import ar.edu.unicen.ringo.persistence.PersistenceService;
 
 public class Activator implements BundleActivator {
 
-	private BundleContext bc = null;
-	private ServiceTracker<HttpService, HttpService> httpTracker = null;
-	private AgentApplication jaxRsApplication = null;
-	private AgentService service = new AgentService();
-	
+    private BundleContext bc = null;
+    private ServiceTracker<HttpService, HttpService> httpTracker = null;
+    private ServiceTracker<PersistenceService, PersistenceService> persistenceTracker = null;
+    private AgentApplication jaxRsApplication = null;
+    private AgentService service = new AgentService();
+
     public void start(BundleContext context) throws Exception {
-    	jaxRsApplication = new AgentApplication(service);
-		httpTracker = new ServiceTracker<HttpService, HttpService>(bc,
-				HttpService.class, null) {
+        this.bc = context;
+        jaxRsApplication = new AgentApplication(service);
 
-					@Override
-					public HttpService addingService(
-							ServiceReference<HttpService> reference) {
-						HttpService httpService = super.addingService(reference);
-						if (httpService == null) {
-							return null;
-						}
-						httpService.registerServlet("/api", new ServletContainer(), null, null);
-						return httpService;
-					}
+        persistenceTracker = new ServiceTracker<>(context,
+                PersistenceService.class, new PersistenceServiceTracker(
+                        context, service));
+        persistenceTracker.open();
 
-					@Override
-					public void removedService(
-							ServiceReference<HttpService> reference,
-							HttpService service) {
-						// TODO Auto-generated method stub
-						super.removedService(reference, service);
-					}
-		};
+        httpTracker = new ServiceTracker<HttpService, HttpService>(bc,
+                HttpService.class, new HttpServiceTracker(context,
+                        jaxRsApplication));
+        httpTracker.open();
     }
 
     public void stop(BundleContext context) throws Exception {
+        httpTracker.close();
+        persistenceTracker.close();
     }
 
 }
