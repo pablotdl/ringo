d1f2ccfe5bc20b39349a81be1e327b4e9510338d
Pablo Saavedra <pablo.a.saavedra@gmail.com>
2013-06-25 00:30:35 -0300

    Implemented elastic search persistence  bundle activator

--- es-persistence/src/main/java/ar/edu/unicen/ringo/elasticsearch/Activator.java
+++ es-persistence/src/main/java/ar/edu/unicen/ringo/elasticsearch/Activator.java
@@ -1,16 +1,51 @@
 package ar.edu.unicen.ringo.elasticsearch;
 
+import java.util.Hashtable;
+import java.util.Properties;
+
+import org.elasticsearch.client.Client;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
+import ar.edu.unicen.ringo.elasticsearch.internal.ElasticSearchClientFactory;
+import ar.edu.unicen.ringo.elasticsearch.internal.TransportClientFactoryImpl;
+import ar.edu.unicen.ringo.persistence.PersistenceService;
+
+/**
+ * Activates or deactivates the bundle, by publishing the
+ * {@link PersistenceService} implementation and managing the underlying client
+ * lifecycle.
+ * @author psaavedra
+ */
 public class Activator implements BundleActivator {
 
+    private static final String DEFAULT_FACTORY = TransportClientFactoryImpl.class.getName();
+
+    private Client client;
+
+    private ServiceRegistration<PersistenceService> handle;
+
+    /**
+     * Starts the bundle.
+     */
     public void start(BundleContext context) throws Exception {
-        // TODO add activation code here
+        ElasticSearchClientFactory factory = Class.forName(DEFAULT_FACTORY)
+                .asSubclass(ElasticSearchClientFactory.class).newInstance();
+        Properties props = new Properties();
+        this.client = factory.createClient(props);
+        PersistenceService service;
+        service = new ElasticSearchPersistenceService(client);
+        handle = context.registerService(PersistenceService.class, service,
+                new Hashtable<String, Object>());
     }
 
+    /**
+     * Stops the bundle.
+     */
     public void stop(BundleContext context) throws Exception {
-        // TODO add deactivation code here
+        this.handle.unregister();
+        this.client.close();
     }
 
 }
