95b10f2654413fd40131be5feadace8a530b72f3
Pablo Saavedra <pablo.s@ag2technology.com>
2013-11-05 19:06:17 -0300

    Removed console ui from this repo

--- console-ui/src/main/java/ar/edu/unicen/ringo/console/model/Identificable.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unicen.ringo.console.model;
-
-public interface Identificable {
-
-    String getId();
-
-    void setId(String id);
-
-}
--- console-ui/src/main/webapp/WEB-INF/pages/common/header/search.jsp
+++
@@ -1,6 +0,0 @@
-
-<!--Search-->
-<form class="search" action="#">
-	<input type="text" name="q" placeholder="Search...">
-</form>
-<!--Search END-->
