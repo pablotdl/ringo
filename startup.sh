15ac765dc10ccccdebb42da858efe7fa5ffd0e38
Pablo Saavedra <pablo.s@ag2technology.com>
2014-02-20 00:14:48 -0300

    Removed curl invocation from script

--- startup.sh
+++ startup.sh
@@ -8,13 +8,9 @@ fi
 cd "$( dirname "$0" )"
 
 cd agent
-mvn antrun:run -P run-on-felix -Dagent.sla=$1 -Dagent.nod=$2 -Des.host=$3 -Des.post=$4 & 
+mvn antrun:run -P run-on-felix -Dagent.sla=$1 -Dagent.nod=$2 -Des.host=$3 -Des.port=$4 & 
 cd ..
 
 cd  mock-app
 mvn jetty:run
-cd ..
-
-sleep 20
-curl -XGET http://localhost:8081/mock-app/generator/start
 
