d6df4a35f8a4b7ebd6390e514cfe87c88b4e9916
Pablo Saavedra <pablo.s@ag2technology.com>
2014-02-19 23:50:20 -0300

    Replaced pushd and popd since they don't work

--- startup.sh
+++ startup.sh
@@ -7,13 +7,13 @@ fi
 
 cd "$( dirname "$0" )"
 
-pushd agent
+cd agent
 mvn antrun:run -P run-on-felix -Dagent.sla=$1 -Dagent.nod=$2 -Des.host=$3 -Des.post=$4 & 
-popd
+cd ..
 
-pushd mock-app
+cd  mock-app
 mvn jetty:run
-popd
+cd ..
 
 sleep 20
 curl -XGET http://localhost:8081/mock-app/generator/start
