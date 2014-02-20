2a3a9f4b6330afd9f6df5a1ead7de04baef2835f
Pablo Saavedra <pablo.s@ag2technology.com>
2014-02-20 00:35:17 -0300

    Several improvements to the startup script

--- startup.sh
+++ startup.sh
@@ -5,10 +5,18 @@ if [ "$#" -ne 4 ]; then
     exit 1
 fi
 
+echo "Starting up monitoring node"
+echo "SLA: $1"
+echo "Node: $2"
+echo "ES host: $3"
+echo "ES port: $4"
+
+
 cd "$( dirname "$0" )"
+git pull
 
 cd agent
-mvn antrun:run -P run-on-felix -Dagent.sla=$1 -Dagent.nod=$2 -Des.host=$3 -Des.port=$4 & 
+mvn antrun:run -P run-on-felix -Dagent.sla=$1 -Dagent.node=$2 -Des.host=$3 -Des.port=$4 & 
 cd ..
 
 cd  mock-app
